45e1aec275e1dc2a8300f2fd58bf4735a3362198
Jacob Bates <jacob.bates@gmail.com>
2022-10-24 23:21:17 -0400

    Resize to 64x64, but no scoring

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import pgzrun, pygame
 import random
 
-draw_once=True
+WIDTH = 64
+HEIGHT = 64
+
+draw_once = True
 score = 0
 game_over = False
 velocity_y = 0
-gravity = 1
+
+gravity = 0.25
+jump_force = -3
+run_speed = 1
 
 makey = Actor('makey')
-makey.x = 100
-makey.y = 467
+makey.x = 10
+makey.y = 45
 
 obstacles = []
 obstacles_timeout = 0
 
 def on_key_down(key):
 	if key == keys.F:
-		screen.surface = pygame.display.set_mode((640,640), pygame.FULLSCREEN)
+		screen.surface = pygame.display.set_mode((WIDTH,HEIGHT), pygame.FULLSCREEN)
 	elif key == keys.W:
-		screen.surface = pygame.display.set_mode((640,640))
+		screen.surface = pygame.display.set_mode((WIDTH,HEIGHT))
 
 def update():
 	global score, game_over, velocity_y, gravity, obstacles_timeout, obstacles
@@ -26,25 +32,25 @@ def update():
 	obstacles_timeout += 1
 	if obstacles_timeout > 50:
 		actor = Actor('cactus')
-		actor.x = 640
-		actor.y = 472
+		actor.x = 64
+		actor.y = 45
 		obstacles.append(actor)
 		obstacles_timeout = 0
 
 	for actor in obstacles:
-		actor.x -= 8
-		if actor.x < -50:
+		actor.x -= run_speed
+		if actor.x < -10:
 			obstacles.remove(actor)
 			score += 1
 
 	if keyboard.up:
-		velocity_y = -15
+		velocity_y = jump_force
 
 	makey.y += velocity_y
 	velocity_y += gravity
-	if makey.y > 467:
+	if makey.y > 45:
 		velocity = 0
-		makey.y = 467
+		makey.y = 45
 
 	if makey.collidelist(obstacles) != -1:
 		game_over = True
@@ -53,22 +59,22 @@ def draw():
 	global score, game_over, draw_once
 
 	if draw_once:
-		screen.surface = pygame.display.set_mode((640,640), pygame.FULLSCREEN)
+		screen.surface = pygame.display.set_mode((WIDTH,HEIGHT), pygame.FULLSCREEN)
 		draw_once = False
 
 	# Draw sky
-	screen.draw.filled_rect(Rect(0,0,640,640), (0,0,0))
+	screen.draw.filled_rect(Rect(0,0,64,64), (0,0,0))
 	# Draw ground
-	screen.draw.filled_rect(Rect(0,500,640,140), (0,255,0))
+	screen.draw.filled_rect(Rect(0,50,64,14), (0,255,0))
 
-	if game_over:
-		screen.draw.text('Game Over', centerx=320, centery=320, color=(255,255,255), fontsize=60)
-		screen.draw.text('Score: ' + str(score), centerx=320, centery=380, color=(255,255,255), fontsize=40)
-	else:
-		makey.draw()
-		screen.draw.text("Score: " + str(score), (15,10), color=(255,255,255), fontsize=40)
+	#if game_over:
+	#	screen.draw.text('Game Over', centerx=320, centery=320, color=(255,255,255), fontsize=60)
+	#	screen.draw.text('Score: ' + str(score), centerx=320, centery=380, color=(255,255,255), fontsize=40)
+	#else:
+	makey.draw()
+	#screen.draw.text("Score: " + str(score), (15,10), color=(255,255,255), fontsize=40)
 
-		for actor in obstacles:
-			actor.draw();
+	for actor in obstacles:
+		actor.draw();
 
 pgzrun.go()
